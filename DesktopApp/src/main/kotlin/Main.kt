12381dc9c0f0eab1ae97a29e47be47d7e65c534a
TerrificTable55 <omegagaming189gmail.com>
2022-06-06 16:18:33 +0200

    Main File

--- DesktopApp/src/main/kotlin/Main.kt
+++ DesktopApp/src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ import gui.AppFrame
 import javax.swing.JFrame
 
 fun main() {
-    val frame: AppFrame = AppFrame()
+    val frame = AppFrame()
     frame.title = "Socke ToDo"
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
