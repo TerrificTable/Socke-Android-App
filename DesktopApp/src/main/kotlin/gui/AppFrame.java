459f437dd3a042f8e02d61da5d5c1fac180150f0
TerrificTable55 <omegagaming189gmail.com>
2022-06-06 15:55:06 +0200

    Added JFrame base

---
+++ DesktopApp/src/main/kotlin/gui/AppFrame.java
@@ -0,0 +1,32 @@
+/*
+ * Created by JFormDesigner on Mon Jun 06 15:54:18 CEST 2022
+ */
+
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author unknown
+ */
+public class AppFrame extends JFrame {
+    public AppFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        contentPane.setPreferredSize(new Dimension(665, 390));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
